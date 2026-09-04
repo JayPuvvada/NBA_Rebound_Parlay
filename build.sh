03638e2474938531cde43c80557d7d6e918fca36
Jay Puvvada <jaypuvvada09@gmail.com>
2026-09-03 22:12:52 -0500

    fix push

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # 1. Install Python dependencies
-pip3 install -r requirements.txt
+# Temporarily clear proxy variables so pip can reach PyPI without a 407 authentication error
+HTTP_PROXY="" HTTPS_PROXY="" http_proxy="" https_proxy="" pip3 install -r requirements.txt
 
 # 2. Build the React Frontend
 cd frontend
